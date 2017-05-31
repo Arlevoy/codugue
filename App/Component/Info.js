fb09c6aabb7d85abc7a1083720272e75cbb930f7
arlevoy <arthurl@bam.tech>
2017-05-31 13:41:12 +0200

    Add map on info page

--- App/Component/Info.js
+++ App/Component/Info.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+
 
 
 import {
@@ -9,13 +11,60 @@ import {
   Image,
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    marginTop: 60,
+    flexDirection: 'column',
+    backgroundColor: 'transparent',
+  },
+
+  mapContainer:{
+      flex:1,
+  },
+
+  textContainer:{
+      flex:2,
+  },
+  marker: {
+    backgroundColor: "#550bbc",
+    padding: 5,
+    borderRadius: 5,
+  },
+});
+
 class Info extends Component{
     render(){
-        return(
-                <View>
-                    <Text>Texte</Text>
-                        
 
+    const markers = [
+            {
+                latitude: 48.8269986,
+                longitude: 2.3136804000000666,
+                title: 'Foo Place',
+                subtitle: '1234 Foo Drive'
+            }
+];
+        return(
+                <View
+                style={styles.mainContainer}>
+                    <MapView
+                        style={styles.mapContainer}
+                        initialRegion={{
+                        latitude: 48.8269986,
+                        longitude: 2.3136804000000666,
+                        latitudeDelta: 0.002,
+                        longitudeDelta: 0.001,
+                        }}>
+                            <Marker {...coordinate={
+                latitude: 48.8269986,
+                longitude: 2.3136804000000666,
+                }} >
+                            <View style={styles.marker}>
+                            <Text >MPAA</Text>
+                            </View>
+                            </Marker>
+                        </MapView>
+                    <Text style={styles.textContainer}> Bonjour </Text>
                 </View>
 
         );
